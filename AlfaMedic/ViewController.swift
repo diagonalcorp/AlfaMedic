717441bb249b656fcf01b12a5d0b98597c21c52c
Luis Manuel Prieto Almeida <lm@diagonal.ae>
2018-10-17 01:25:04 +0200

    Initial Commit

---
+++ AlfaMedic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlfaMedic
+//
+//  Created by Luis Manuel Prieto Almeida on 17/10/18.
+//  Copyright © 2018 Diagonal World Corporation OÜ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

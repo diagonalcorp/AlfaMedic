deb0de2176e65fac6b4c1c802849b1e0f8b13c6b
Luis Manuel Prieto Almeida <lm@diagonal.ae>
2018-10-24 14:36:28 +0200

    Initial commit

--- AlfaMedic/ViewController.swift
+++ AlfaMedic/ViewController.swift
@@ -2,19 +2,38 @@
 //  ViewController.swift
 //  AlfaMedic
 //
-//  Created by Luis Manuel Prieto Almeida on 17/10/18.
+//  Created by Luis Manuel Prieto Almeida for CEMEDIC Group on 17/10/18.
 //  Copyright © 2018 Diagonal World Corporation OÜ. All rights reserved.
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var alfam1: UIView? = nil
+    var webView: WKWebView?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let myURL = URL(string: "http://www.app.alfamedic.es/")
+        let myRequest = URLRequest(url: myURL!)
+        _ = webView?.load(myRequest)
     }
-
-
+    
+    override func loadView() {
+        super.loadView()
+        self.webView = WKWebView()
+        self.view = self.webView
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
 }
-
